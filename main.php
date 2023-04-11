46e6410771d39ee60bc38d0e7b4c2f978a57e985
SmolFennec <50549928@student.southessex.ac.uk>
2023-04-11 15:24:18 +0100

    added literally just 1 picture

--- main.php
+++ main.php
@@ -1,4 +1,6 @@
 <link rel="stylesheet" href="css/main.css"/>
 
-<div class="main"></div>
+<div class="main">
+    <img style="width:100%;height: 100%;" src="img/food.jpg" alt="food" class="food"/>
+</div>
 <div class="Ebox"></div>
