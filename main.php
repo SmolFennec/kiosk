aa55301fad5b37e93b43a4f0ba18a30fbac09a51
William McCusker <Smurf1.bm@gmail.com>
2023-04-14 14:45:25 +0100

    adjusted css and added basics of order page

--- main.php
+++ main.php
@@ -1,6 +1,14 @@
+<link rel="stylesheet" href="css/all.css"/>
 <link rel="stylesheet" href="css/main.css"/>
 
+<h1>restaurant logo here</h1>
 <div class="main">
     <img style="width:100%;height: 100%;" src="img/food.jpg" alt="food" class="food"/>
+    <a href="order.php">
+        <div class="popup">
+            <p>Start order</p>
+        </div>
+    </a>
 </div>
+
 <div class="Ebox"></div>
---
+++ order.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="css/all.css"/>
+<link rel="stylesheet" href="css/order.css"/>
+
+<h1>restaurant logo here</h1>
+<div class="side">
+    <div class="sidebox"></div>
+    <div class="sidebox"></div>
+    <div class="sidebox"></div>
+    <div class="sidebox"></div>
+    <div class="sidebox"></div>
+    <div class="sidebox"></div>
+    <div class="sidebox"></div>
+
+</div>
+<div class="main">
+
+
+</div>
+
+<div class="Ebox"></div>
