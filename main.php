1dc6c737c8422d1e1a957c9027b2aaf7d25fdfc4
SmolFennec <50549928@student.southessex.ac.uk>
2023-04-18 15:06:59 +0100

    small changes? i dont remember

--- main.php
+++ main.php
@@ -3,7 +3,8 @@
 
 <h1>restaurant logo here</h1>
 <div class="main">
-    <img style="width:100%;height: 100%;" src="img/food.jpg" alt="food" class="food"/>
+    <img style="width:100%;height: 100%;"
+         src="img/food.jpg" alt="food" class="food"/>
     <a href="order.php">
         <div class="popup">
             <p>Start order</p>
--- order.php
+++ order.php
@@ -3,18 +3,27 @@
 
 <h1>restaurant logo here</h1>
 <div class="side">
-    <div class="sidebox"></div>
-    <div class="sidebox"></div>
-    <div class="sidebox"></div>
-    <div class="sidebox"></div>
-    <div class="sidebox"></div>
-    <div class="sidebox"></div>
-    <div class="sidebox"></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
 
 </div>
+<div class="cancelbox">
+    <p>Cancel</p>
+</div>
 <div class="main">
+    <div class="radbox">
 
+    </div>
+
+</div>
 
 </div>
 
 <div class="Ebox"></div>
+
+
