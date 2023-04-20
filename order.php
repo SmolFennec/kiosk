48d5fb6e134e5be712e669f58b286a155585bde1
50549928 <505499285@student.southessex.ac.uk>
2023-04-20 12:15:39 +0100

    ¯\_(ツ)_/¯

--- order.php
+++ order.php
@@ -3,27 +3,27 @@
 
 <h1>restaurant logo here</h1>
 <div class="side">
-    <div class="sidebox"><p>Food category</p></div>
-    <div class="sidebox"><p>Food category</p></div>
-    <div class="sidebox"><p>Food category</p></div>
-    <div class="sidebox"><p>Food category</p></div>
-    <div class="sidebox"><p>Food category</p></div>
-    <div class="sidebox"><p>Food category</p></div>
-    <div class="sidebox"><p>Food category</p></div>
+  <div class="sidebox"><p>Food category</p></div>
+  <div class="sidebox"><p>Food category</p></div>
+  <div class="sidebox"><p>Food category</p></div>
+  <div class="sidebox"><p>Food category</p></div>
+  <div class="sidebox"><p>Food category</p></div>
+  <div class="sidebox"><p>Food category</p></div>
+  <div class="sidebox"><p>Food category</p></div>
 
 </div>
-<div class="cancelbox">
+<a href="main.php">
+  <div class="cancelbox">
     <p>Cancel</p>
-</div>
+  </div>
+</a>
 <div class="main">
-    <div class="radbox">
-
-    </div>
+  <div class="radbox">
+    <img style="width:100%;height: 100%;" src="img/burgs.jpg" alt="food" class="food"/>
+  </div>
 
 </div>
 
 </div>
 
 <div class="Ebox"></div>
-
-
