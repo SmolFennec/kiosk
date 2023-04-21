54874c5c6ace8a54e8dbbd06ea3f80b77568b6d2
SmolFennec <50549928@student.southessex.ac.uk>
2023-04-21 11:39:25 +0100

    added banner and buttons(non-functional)

--- order.php
+++ order.php
@@ -3,24 +3,73 @@
 
 <h1>restaurant logo here</h1>
 <div class="side">
-  <div class="sidebox"><p>Food category</p></div>
-  <div class="sidebox"><p>Food category</p></div>
-  <div class="sidebox"><p>Food category</p></div>
-  <div class="sidebox"><p>Food category</p></div>
-  <div class="sidebox"><p>Food category</p></div>
-  <div class="sidebox"><p>Food category</p></div>
-  <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
+    <div class="sidebox"><p>Food category</p></div>
 
 </div>
 <a href="main.php">
-  <div class="cancelbox">
-    <p>Cancel</p>
-  </div>
+    <div class="cancelbox">
+        <p>Cancel</p>
+    </div>
 </a>
 <div class="main">
-  <div class="radbox">
-    <img style="width:100%;height: 100%;" src="img/burgs.jpg" alt="food" class="food"/>
-  </div>
+    <div class="radbox">
+        <img style="width:100%;height: 100%;" src="img/banner.jpg" alt="food"/>
+    </div>
+
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
+    <div class="food">
+        <img src="img/burg.jpg" alt="food"/>
+        <p>burger</p>
+    </div>
 
 </div>
 
