400c52f8b6936d983e7ba7773239e64a56a05304
SmolFennec <50549928@student.southessex.ac.uk>
2023-04-25 11:56:11 +0100

    order page complete

---
+++ JS/order butts.js
@@ -0,0 +1,21 @@
+let ButtArray = ["burger", "sandwich", "beef", "burger", "burger", "burger", "burger", "burger", "burger", "burger", "burger", "burger"];
+let fooddict = {"burger":5.99, "sandwich":4.99, "beef":6.99};
+let order = [];
+let total = 0;
+
+function clicked(Bnum) {
+    let Butt = ButtArray[Bnum];
+    order.push(Butt);
+    document.getElementById("L3").innerHTML = document.getElementById("L2").innerHTML;
+    document.getElementById("L2").innerHTML = document.getElementById("L1").innerHTML;
+    document.getElementById("L1").innerHTML = Butt +"                                              £" + fooddict[Butt];
+
+    total += fooddict[Butt];
+
+    document.getElementById("total").innerHTML = "£" + total.toFixed(2);
+}
+
+function finalise(){
+    localStorage.setItem("order", JSON.stringify(order));
+    window.location.href = "final.php";
+}
--- order.php
+++ order.php
@@ -1,15 +1,31 @@
 <link rel="stylesheet" href="css/all.css"/>
 <link rel="stylesheet" href="css/order.css"/>
+<script src="JS/order butts.js"></script>
 
 <h1>restaurant logo here</h1>
 <div class="side">
+    <a href="order.php">
     <div class="sidebox"><p>Food category</p></div>
+    </a>
+    <a href="order.php">
     <div class="sidebox"><p>Food category</p></div>
+    </a>
+    <a href="order.php">
     <div class="sidebox"><p>Food category</p></div>
+    </a>
+    <a href="order.php">
     <div class="sidebox"><p>Food category</p></div>
+    </a>
+    <a href="order.php">
     <div class="sidebox"><p>Food category</p></div>
+    </a>
+    <a href="order.php">
+    <div class="sidebox"><p>Food category</p>
+    </div>
+    </a>
+    <a href="order.php">
     <div class="sidebox"><p>Food category</p></div>
-    <div class="sidebox"><p>Food category</p></div>
+    </a>
 
 </div>
 <a href="main.php">
@@ -22,51 +38,51 @@
         <img style="width:100%;height: 100%;" src="img/banner.jpg" alt="food"/>
     </div>
 
-    <div class="food">
+    <div class="food" onclick="clicked(0)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(1)">
         <img src="img/burg.jpg" alt="food"/>
         <p>sandwich</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(2)">
         <img src="img/burg.jpg" alt="food"/>
         <p>beef</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(3)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(4)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(5)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(6)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(7)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(8)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(9)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(10)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
-    <div class="food">
+    <div class="food" onclick="clicked(11)">
         <img src="img/burg.jpg" alt="food"/>
         <p>burger</p>
     </div>
@@ -77,13 +93,17 @@
 
 <div class="Ebox">
     <div class="items">
-        <p></p>
+        <p id="L1">.</p>
         <div class="line"></div>
-        <p></p>
+        <p id="L2">.</p>
         <div class="line"></div>
-        <p></p>
+        <p id="L3">.</p>
     </div>
-    <div class="pay">
+    <div class="pay" onclick="finalise()">
         <img src="img/pay.jpg" alt="pay"/>
+    </div>
+    <div class="total">
+        <p>Total</p>
+        <p id="total">0</p>
     </div>
 </div>
